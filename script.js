74993cc88130dc25b713d112f95b5b523f5158ea
Dan <dan.belfield@outlook.com>
2019-07-05 22:30:05 +0100

    player selection, button A working only

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 
-var computerSelection = document.querySelector('.compOutput');
+// COMPUTER SELECTION
 
-function compSelection() {
+let computerSelection = document.querySelector('.compOutput');
+
+function compPicked() {
 
     let randomNumber = Math.random() * 3;
     console.log(randomNumber);
 
     if (randomNumber < 1) {
-        computerSelection.textContent = 'Rock';
+        computerSelection.textContent = 'Computer selected: Rock';
         console.log('rock');
     } else if (randomNumber < 2) {
-        computerSelection.textContent = 'Paper';
+        computerSelection.textContent = 'Computer selected: Paper';
         console.log('paper');
     } else if (randomNumber <=3 ) {
-        computerSelection.textContent = 'Scissors';
+        computerSelection.textContent = 'Computer selected: Scissors';
         console.log('scissors');
     } else {
         computerSelection.textContent = "Computer Selection Error"
+    }
+
+}
+
+// PLAYER SELECTION
+
+let playerSelection = document.querySelector('.playerOutput');
+let selected = document.querySelector('button');
+
+selected.addEventListener('click', playerPicked);
+
+function playerPicked() {
+    
+    let choice = selected.value;
+
+    if (choice === 'rock') {
+        playerSelection.textContent = 'You selected: Rock';
+    } else if (choice === 'paper') {
+        playerSelection.textContent = 'You selected: Paper';
+    } else if (choice === 'scissors') {
+        playerSelection.textContent = 'You selected: Scissors';
+    } else {
+        playerSelection.textContent = "Player Selection Error"
     }
 
 }
