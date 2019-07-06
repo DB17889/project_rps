048712dde683b0258fc60280207681395079a56a
Dan <dan.belfield@outlook.com>
2019-07-06 21:51:54 +0100

    next... scores

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
+
+document.addEventListener('click', playerPicked);                       // Adds an event listener to the whole document looking for a click, then running the function if a click is found.
+
 // PLAYER SELECTION
 
 let playerSelection = document.querySelector('.playerOutput');
-
-document.addEventListener('click', playerPicked);                   // Adds an event listener to the whole document looking for a click, then running the function if a click is found.
 
 function playerPicked(btnclck) {                                    
 
@@ -19,10 +20,11 @@ function playerPicked(btnclck) {
             playerSelection.textContent = 'You selected: Scissors';
             playRound("scissors", compPicked());
         } else {
-            playerSelection.textContent = 'Player Selection Error'
+            console.log('Error 1');
         }
     }
 }
+
 
 // COMPUTER SELECTION
 
@@ -31,23 +33,91 @@ let computerSelection = document.querySelector('.compOutput');
 function compPicked() {
 
     let randomNumber = Math.random() * 3;
-    console.log(randomNumber);
+    //console.log(randomNumber);  - remove after testing
 
     if (randomNumber < 1) {
         computerSelection.textContent = 'Computer selected: Rock';
-        console.log('rock');
+        return 'rock';
+        //console.log('rock'); - remove after testing
     } else if (randomNumber < 2) {
         computerSelection.textContent = 'Computer selected: Paper';
-        console.log('paper');
+        return 'paper';
+        //console.log('paper'); - remove after testing
     } else if (randomNumber <=3 ) {
         computerSelection.textContent = 'Computer selected: Scissors';
-        console.log('scissors');
+        return 'scissors'
+        //console.log('scissors'); - remove after testing
     } else {
-        computerSelection.textContent = "Computer Selection Error"
+        console.log('Error 2');
     }
 
 }
 
-function playRound(playerSelection, computerSelection) {
-    console.log(playerSelection, computerSelection)
+
+
+
+
+
+// PLAY ONE ROUND
+
+function startRound(btnclck) {
+    if (btnclck.target.classList.contains("btn")){  
+        playRound;
+        scores;
+    }
 }
+
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection, computerSelection);
+
+        if (playerSelection === 'rock') {
+            if (computerSelection === 'rock') {
+                roundOutput.textContent = 'It\'s a draw';
+            } else if (computerSelection === 'paper') {
+                roundOutput.textContent = 'Paper beats Rock! You Lose!';
+            } else if (computerSelection === 'scissors') {
+                roundOutput.textContent = 'Rock beats Scissors! You Win!';
+            } else {
+                console.log('Error 3');
+            }
+        } else if (playerSelection === 'paper') {
+            if (computerSelection === 'rock') {
+                roundOutput.textContent = 'Paper beats Rock! You Win!'
+            } else if (computerSelection === 'paper') {
+                roundOutput.textContent = 'It\'s a draw';
+            } else if (computerSelection === 'scissors') {
+                roundOutput.textContent = 'Scissors beats Paper! You Lose!'
+            } else {
+                console.log('Error 4');
+            }
+        } else if (playerSelection === 'scissors') {
+            if (computerSelection === 'rock') {
+                roundOutput.textContent = 'Rock beats Scissors! You Lose!'
+            } else if (computerSelection === 'paper') {
+                roundOutput.textContent = 'Scissors beats Paper! You Win!'
+            } else if (computerSelection === 'scissors') {
+                roundOutput.textContent = 'It\'s a draw';
+            } else {
+                console.log('Error 5');
+            }
+        }
+    }
+
+let roundOutput = document.querySelector('.roundOutput');
+let playerScore = document.querySelector('.playerScore');
+let compScore = document.querySelector('.compScore');
+
+
+
+// KEEPING SCORE - CANT GET THIS TO WORK!
+
+let PScore = '1';
+let CScore = '0';
+
+function scores() {
+    if (PScore > 1 || CScore > 1) {  
+        playerScore.textContent = 'Player Score: ' + PScore;
+        compScore.textContent = 'Computer Score: ' + CScore;
+    }
+}
+
